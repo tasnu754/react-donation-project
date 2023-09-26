280885f29cba73ba081952984c8d01ee19d30d66
Tasnuva Islam <tasnuvamarziya60@gmail.com>
2023-09-26 15:50:22 +0600

    Final

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
   There are all category cards in home section by default. If you click any card then it will take you another page where you can see the details of clicked card. And there is a doonation button, when button clicked, the card will add into donation section.
 
 * I have store the data in local storage. When a button will clicked, the data of that button will be stored
-  in the local storage using localStorage.setItem() function. And then I get the data by using localStorage.getItem() function and show the cards in donation section.
+  in the local storage using localStorage.setItem() function. And then I get the data by using localStorage.getItem() function and show the cards in donation section. And also there is a see all button in donation section. When cards length is greater than 4, the button will appear and once click the button the button will hidden and all cards will be shown.
 
 * In Statistic section I have added a pie chart which is showing your donation against the total donation in 
-  persantage. And the search button in the home section is functional. When you search a particular type of category, you will see only the cards of that category. And also there is a see all button in donation section. When cards length is greater than 4, the button will appear and once click the button the button will hidden and all cards will be shown.
+  persantage. And the search button in the home section is functional. When you search a particular type of category, you will see only the cards of that category.
--- src/Components/DonationAdd/DonationAdd.jsx
+++ src/Components/DonationAdd/DonationAdd.jsx
@@ -20,7 +20,7 @@ const DonationAdd = () => {
 
     return (
       <div>
-        <div className="w-[85%]  mx-auto grid grid-cols-1 md:grid-cols-2 gap-6">
+        {/* <div className="w-[85%]  mx-auto grid grid-cols-1 md:grid-cols-2 gap-6">
           {isSeeAll
             ? addCards.map((donateCards, idx) => (
                 <DonateCartAdd
@@ -46,6 +46,45 @@ const DonationAdd = () => {
             >
               See All
             </button>
+          </div>
+        )} */}
+
+        {addCards <= 0 ? (
+          <div className="h-[500px]  pt-44 flex align-middle justify-center">
+            <p className="text-4xl font-bold">No Donation yet</p>
+          </div>
+        ) : (
+          <div>
+            {" "}
+            <div className="w-[85%]  mx-auto grid grid-cols-1 md:grid-cols-2 gap-6">
+              {isSeeAll
+                ? addCards.map((donateCards, idx) => (
+                    <DonateCartAdd
+                      key={idx}
+                      donateCards={donateCards}
+                    ></DonateCartAdd>
+                  ))
+                : addCards
+                    .slice(0, 4)
+                    .map((donateCards, idx) => (
+                      <DonateCartAdd
+                        key={idx}
+                        donateCards={donateCards}
+                      ></DonateCartAdd>
+                    ))}
+            </div>
+            {addCards.length > 4 && (
+              <div className="w-[85%]  mx-auto flex justify-center my-10">
+                <button
+                  onClick={handleSeeAll}
+                  className={`w-[110px] ${
+                    isSeeAll && "hidden"
+                  }   btn btn-active btn-secondary`}
+                >
+                  See All
+                </button>
+              </div>
+            )}
           </div>
         )}
       </div>
